85c69ae2647e16b496815c11e63e3a83507ffa82
Enrico Bianchi <enrico.bianchi@gmail.com>
2015-04-07 17:25:48 +0200

    Added configuration file

--- ec2service.py
+++ ec2service.py
@@ -3,7 +3,12 @@
 __author__ = 'ebianchi'
 
 import bottle
+import configparser
 import json
+import sys
+
+# FIXME: for commodity, config file is declared as global variable. Change in future
+CFG = configparser.ConfigParser()
 
 @bottle.route("/")
 def hello():
@@ -26,4 +31,10 @@ def machine_command(name):
     return json.dumps({"result": "ko", "message": "Stop command for machine " + name + " not implemented"})
 
 if __name__ == "__main__":
-    bottle.run(host='localhost', port=8080, debug=False)
+    try:
+        CFG.read(sys.argv[1])
+    except:
+        print("Usage: " + sys.argv[0] + " <configfile>")
+        sys.exit(1)
+
+    bottle.run(host=CFG.get("service", "listen"), port=CFG.get("service", "port"), debug=False)
